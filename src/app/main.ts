4e40778bbbe4349cdb09be0251fcb79cde9566b5
Cristina Hernández <achh17@gmail.com>
2022-09-12 18:48:39 -0500

    added new code for random products using Faker's library.

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,28 @@
-import { addProduct } from './products/product.service';
+import {faker} from '@faker-js/faker';
 
-addProduct({
-    id: '1',
-    title: 'p1',
-    createdAt: new Date(),
-    updatedAt: new Date,
-    stock: 12,
+import { addProduct, products } from './products/product.service';
+
+for (let index = 0; index < 50; index++) {
+  addProduct({
+    id: faker.datatype.uuid(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.imageUrl(),
+    color: faker.color.human(),
+    size: faker.helpers.arrayElement(['M', 'S', 'XL', 'L']),
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements(),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.datatype.number({min: 10, max: 100}),
     category: {
-        id:'123',
-        name:'c1',
-        createdAt: new Date,
-        updatedAt: new Date,
+      id: faker.datatype.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
     }
-});
+  });
+}
+
+console.log(products);
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -5,3 +5,25 @@ export const products : Product[] = [];
 export const addProduct = (data: Product) => {
     products.push(data);
 }
+
+export const updateProduct = (id: string, changes:Product) => {
+    const index: number = getIndex(id)
+    const product: Product = products[index]
+   index === -1 && throwError('Product not Found')
+  
+    products[index] = {
+      ...product,
+      ...changes
+    }
+    return products[index]
+}
+
+const getIndex = (id: string) => products.findIndex(product => product.id === id)
+const throwError = (error: string) => new Error(error)
+
+export const findProduct = (id:string) => {
+    const index: number = getIndex(id)
+    index === -1 && throwError('Product not Found')
+    return products[index]
+  }
+
