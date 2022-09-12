dac6aa529df48cdd9f0304d2f4e108ade62c97c6
Cristina Hernández <achh17@gmail.com>
2022-09-12 16:41:48 -0500

    added new file for Interface and I understood the difference between interface and type.

---
+++ src/interfaces.ts
@@ -0,0 +1,29 @@
+type Sizes = 'S'| 'M'|'L'|'XL' // type son mas cortas y puntuales 
+/* type Product = {
+    id: string |number;
+    title: string;
+    createdAt: Date;
+    stock: number;
+    size?: Sizes;
+} */
+
+// interface se utilizan para un conjunto de atributos de valores
+
+interface Product  {
+    id: string |number;
+    title: string;
+    createdAt: Date;
+    stock: number;
+    size?: Sizes;
+}
+const products : Product[] = [];
+
+products.push({
+    id: '1',
+    title: 'p1',
+    createdAt: new Date(),
+    stock: 12,
+});
+
+
+    
